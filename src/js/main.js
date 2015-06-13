6803879df0bb8daf1b9eb71a544f50cfe8c74f05
Kim Mattiko <amberlie@rocketmail.com>
2015-06-13 15:52:16 -0400

    added the data in from API

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,17 @@
-$.getJSON('/../../api/brewery-db/zombie-killer.json')
- .then(function(message){
-   console.log(message);
+$.getJSON('/../../api/brewery-db/zombiekiller.json')
+ .then(function(zombiekiller){
+   console.log(zombiekiller);
 
-   $beername = $('.beername');
-   $beername.text(message.name);
+   $beername = $('#beername');
+   $beername.text(zombiekiller.data.name);
+
+   $abvvalue = $('#abvvalue');
+   $abvvalue.text(zombiekiller.data.abv);
+
+   $describe = $('#describe');
+   $describe.text(zombiekiller.data.description);
+
+   $catdes = $('#catdes');
+   $catdes.text(zombiekiller.data.style.description);
 
  });
