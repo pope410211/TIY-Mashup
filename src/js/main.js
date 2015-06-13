9f9c016aeb2f0c372cf3a4207ca787a15ab29241
Nick Pope <pope410211@gmail.com>
2015-06-13 12:22:07 -0400

    it's a APIIIIII-and it worked for youtube

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,17 @@ $.getJSON('/../../api/brewery-db/zombie-killer.json')
  .then(function(message){
    console.log(message);
 
-   $beername = $('.beername');
-   $beername.text(message.name);
+   $beerName = $('h4.beername');
+   $beerName.text(message.name);
 
  });
 
-$.getJSON('/../../api/youtube/mead.json')
-.then(function(mead){
-  console.log(mead);
+$.getJSON('/../../api/youtube/b-nektar.json')
+.then(function(bnektar){
+  console.log(bnektar);
 
-  
+  $vidTitle = $('h4#nektar');
+  $vidTitle.text(bnektar.title);
+
+
 })
